4da1d84001641766ef462a15ce483947fc9fb79c
lavanon <35270586+lavanon@users.noreply.github.com>
2022-02-10 12:59:00 +0000

    Add some svelte code to show tasks

--- simple-todos-svelte/imports/ui/App.svelte
+++ simple-todos-svelte/imports/ui/App.svelte
@@ -1,11 +1,25 @@
 <script>
-  let counter = 0;
-  const addToCounter = () => {
-    counter += 1;
-  }
+  import Task from './Task.svelte';
+
+  const getTasks = () => ([
+      { _id: 'task_1', text: 'Do this meteor course' },
+      { _id: 'task_2', text: 'Enjoy the process of learning' },
+      { _id: 'task_3', text: 'Be # proud' },
+  ])
 </script>
 
+<div class="container">
+  <header>
+      <h1>Todo List</h1>
+  </header>
 
+  <ul>
+      {#each getTasks() as task (task._id)}
+          <Task task={task} />
+      {/each}
+  </ul>
+</div>
+<!-- 
 <div class="container">
   <h1>Welcome to Meteor!</h1>
 
@@ -19,4 +33,4 @@
     <li><a href="https://docs.meteor.com" target="_blank">Read the Docs</a></li>
     <li><a href="https://forums.meteor.com" target="_blank">Discussions</a></li>
   </ul>
-</div>
+</div> -->
